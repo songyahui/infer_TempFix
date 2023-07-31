9168e137d8cfee828751e8bfaf9340db1f375023
songyahui <songyahuiapp@hotmail.com>
2023-07-31 13:13:28 +0800

    added the spec for all the recource usage

--- spec.c
+++ spec.c
@@ -6,14 +6,29 @@
     Post (ret<0, 𝝐) \/ (ret>=0, open(ret))
     Future (ret>=0, (!close(ret))^* · close(ret) · (_)^* )  @*/
 
+/*@ socket(domain, type, protocol): 
+    Post (ret<0, 𝝐) \/ (ret>=0, socket(domain))
+    Future (ret>=0, (!close(domain))^* · close(domain) · (_)^* )  @*/
 
 /*@ close(handler): 
     Post (TRUE, close(handler)) 
     Future  (TRUE, (!_(handler))^*)  @*/
 
-/*@ socket(domain, type, protocol): 
-    Post (ret<0, 𝝐) \/ (ret>=0, open(domain))
-    Future (ret>=0, (!close(domain))^* · close(domain) · (_)^* )  @*/
+/*@ fopen(path): 
+    Post (ret<0, 𝝐) \/ (ret>=0, fopen(ret))
+    Future (ret>=0, (!fclose(ret))^* · fclose(ret) · (_)^* )  @*/
+
+/*@ fclose(handler): 
+    Post (TRUE, fclose(handler)) 
+    Future  (TRUE, (!_(handler))^*)  @*/
+
+/*@ opendir(path): 
+    Post (ret<0, 𝝐) \/ (ret>=0, opendir(ret))
+    Future (ret>=0, (!closedir(ret))^* · closedir(ret) · (_)^* )  @*/
+
+/*@ closedir(handler): 
+    Post (TRUE, closedir(handler)) 
+    Future  (TRUE, (!_(handler))^*)  @*/
 
 
 // swClient_create
