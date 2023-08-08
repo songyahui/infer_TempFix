1b34d1820200402739cc754cebafdb7bcc82dac8
songyahui <songyahuiapp@hotmail.com>
2023-08-08 15:50:54 +0800

    m

--- spec.c
+++ spec.c
@@ -1,22 +1,84 @@
 
 #define SW_CHANNEL_MIN_MEM (1024*64)
 
-/* malloc(path): 
-    Post (TRUE, malloc(ret))
-    Future (TRUE, (!free(ret))^* · free(ret) · (_)^* ) @*/
+// Resourse leak 
+/*@ open(path): 
+    Post (ret<0, 𝝐) \/ (ret>=0, open(ret))
+    Future (ret>=0, (!close(ret))^* · close(ret) · (_)^* )  @*/
 
-/* free(handler): 
+/*@ socket(domain, type, protocol): 
+    Post (ret<0, 𝝐) \/ (ret>=0, socket(ret))
+    Future (ret>=0, (!close(ret))^* · close(ret) · (_)^* )  @*/
+
+/*@ swSocket_create(arg): 
+    Post (ret<0, 𝝐) \/ (ret>=0, socket(ret))
+    Future (ret>=0, (!close(ret))^* · close(ret) · (_)^* )  @*/
+
+/*@ close(handler): 
+    Post (TRUE, close(handler)) 
+    Future  (TRUE, (!_(handler))^*)  @*/
+
+/*@ fopen(path): 
+    Post (ret<0, 𝝐) \/ (ret>0, fopen(ret))
+    Future (ret>0, (!fclose(ret))^* · fclose(ret) · (_)^* )  @*/
+
+/*@ fclose(handler): 
+    Post (TRUE, fclose(handler)) 
+    Future  (TRUE, (!_(handler))^*)  @*/
+
+/*@ opendir(path): 
+    Post (ret<0, 𝝐) \/ (ret>0, opendir(ret))
+    Future (ret>0, (!closedir(ret))^* · closedir(ret) · (_)^* )  @*/
+
+/*@ closedir(handler): 
+    Post (TRUE, closedir(handler)) 
+    Future  (TRUE, (!_(handler))^*)  @*/
+
+// Memory bugs 
+/*@ malloc(path): 
+    Post (ret=0, 𝝐) \/ (!(ret=0), malloc(ret))
+    Future (!(ret=0), (!free(ret))^* · free(ret) · (_)^* ) \/ (ret=0, (!_(ret))^*) @*/
+
+/*@ free(handler): 
     Post (TRUE, free(handler)) 
-    Future  (TRUE, (!_(handler))^* · (𝝐 \/ (malloc(handler) · (_)^*)))  @*/
+    Future  (TRUE, (!_(handler))^*)  @*/
 
+/*@ swMalloc_alloc(path): 
+    Post (ret=0, 𝝐) \/ (!(ret=0), malloc(ret))
+    Future (!(ret=0), (!free(ret))^* · free(ret) · (_)^* ) \/ (ret=0, (!_(ret))^*) @*/
+
+
+// NPD
+/*@ localtime(t): 
+    Future  (ret=0, (!_(ret))^*)  @*/
+
+/*@ swReactor_get(t): 
+    Future  (ret=0, (!_(ret))^*)  @*/
+// --------------------
 /*@ malloc(path): 
     Post (ret=0, 𝝐) \/ (!(ret=0), malloc(ret))
     Future (ret=0, (!_(ret))^*) @*/
 
-/*@ memset(a, b): 
-    Post (TRUE, memset(a))  @*/
+/*@ swMalloc_alloc(path): 
+    Post (ret=0, 𝝐) \/ (!(ret=0), malloc(ret))
+    Future (ret=0, (!_(ret))^*) @*/
+// --------------------
+/*@ swoole_get_property(a, b): 
+    Future  (ret=0, (!_(ret))^*)  @*/
+// --------------------
+/*@ swServer_connection_verify(a, b): 
+    Future  (ret=0, (!_(ret))^*)  @*/
 
-/*@ p11_array_new(a, b): 
-    Post (TRUE, strcpy(a))  @*/
+/*@ swServer_connection_get(a, b): 
+    Future  (ret=0, (!_(ret))^*)  @*/
+// --------------------
+/*@ swServer_get_worker(a, b): 
+    Future  (ret=0, (!_(ret))^*)  @*/
+
+/*@ swWorker_free(handler): 
+    Post (TRUE, free(handler))  @*/
+
+/*@ kill(handler, b): 
+    Post (TRUE, free(handler))  @*/
 
 
