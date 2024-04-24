9f8e18c4af851d9cda4f296e59cc10ffa884183e
songyahui <songyahuiapp@hotmail.com>
2024-04-24 16:57:01 +0800

    change the path setting

--- TempFix.ml
+++ TempFix.ml
@@ -2,7 +2,10 @@
 let which_system = if String.compare (String.sub (Sys.getcwd()) 0 5 ) "/home" == 0 then 1 else 0 
 let loris1_path = "/home/yahui/future_condition/infer_TempFix/" 
 let mac_path = "/Users/yahuis/Desktop/git/infer_TempFix/"
-let path = if which_system == 1  then loris1_path else mac_path 
+(*let path = if which_system == 1  then loris1_path else mac_path *)
+let path = 
+(Filename.dirname(Filename.dirname(Filename.dirname(Filename.dirname(Sys.getcwd ()))))) ^ "/" 
+
 let output_report =  path ^ "TempFix-out/report.csv" 
 let output_detail =  path ^ "TempFix-out/detail.txt" 
 
--- TempFixDataAnalysis.py
+++ TempFixDataAnalysis.py
@@ -6,7 +6,8 @@ which_system = 1 if os.getcwd()[0:5] == "/home" else 0
 
 loris1_path = "/home/yahui/future_condition/infer_TempFix/" 
 mac_path = "/Users/yahuis/Desktop/git/infer_TempFix/"
-path = loris1_path if which_system == 1 else mac_path 
+path = (os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd ()))))) + "/"  
+
 output_report =  path + "TempFix-out/report.csv" 
 output_detail =  path + "TempFix-out/detail.txt" 
 
